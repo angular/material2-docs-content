57487e5cb70cfce5aff7ea033aadd249fe6afea4
Austin <amcdaniel2@gmail.com>
2017-10-27 17:39:27 +0000

    8993451 chore(docs): adds types column and null checks for description (#7713)

--- examples-package/typings/table-basic/table-basic-example.d.ts
+++ examples-package/typings/table-basic/table-basic-example.d.ts
@@ -1,27 +1,14 @@
-import { DataSource } from '@angular/cdk/collections';
-import { Observable } from 'rxjs/Observable';
-import 'rxjs/add/observable/of';
+import { MatTableDataSource } from '@angular/material';
 /**
  * @title Basic table
  */
 export declare class TableBasicExample {
     displayedColumns: string[];
-    dataSource: ExampleDataSource;
+    dataSource: MatTableDataSource<Element>;
 }
 export interface Element {
     name: string;
     position: number;
     weight: number;
     symbol: string;
-}
-/**
- * Data source to provide what data should be rendered in the table. The observable provided
- * in connect should emit exactly the data that should be rendered by the table. If the data is
- * altered, the observable should emit that new set of data on the stream. In our case here,
- * we return a stream that contains only one set of data that doesn't change.
- */
-export declare class ExampleDataSource extends DataSource<any> {
-    /** Connect function called by the table to retrieve one stream containing the data to render. */
-    connect(): Observable<Element[]>;
-    disconnect(): void;
 }
--- examples-package/typings/table-pagination/table-pagination-example.d.ts
+++ examples-package/typings/table-pagination/table-pagination-example.d.ts
@@ -1,49 +1,20 @@
-import { DataSource } from '@angular/cdk/collections';
-import { MatPaginator } from '@angular/material';
-import { BehaviorSubject } from 'rxjs/BehaviorSubject';
-import { Observable } from 'rxjs/Observable';
-import 'rxjs/add/operator/startWith';
-import 'rxjs/add/observable/merge';
-import 'rxjs/add/operator/map';
+import { MatPaginator, MatTableDataSource } from '@angular/material';
 /**
  * @title Table with pagination
  */
 export declare class TablePaginationExample {
     displayedColumns: string[];
-    exampleDatabase: ExampleDatabase;
-    dataSource: ExampleDataSource | null;
+    dataSource: MatTableDataSource<Element>;
     paginator: MatPaginator;
-    ngOnInit(): void;
+    /**
+     * Set the paginator after the view init since this component will
+     * be able to query its view for the initialized paginator.
+     */
+    ngAfterViewInit(): void;
 }
-export interface UserData {
-    id: string;
+export interface Element {
     name: string;
-    progress: string;
-    color: string;
-}
-/** An example database that the data source uses to retrieve data for the table. */
-export declare class ExampleDatabase {
-    /** Stream that emits whenever the data has been modified. */
-    dataChange: BehaviorSubject<UserData[]>;
-    readonly data: UserData[];
-    constructor();
-    /** Adds a new user to the database. */
-    addUser(): void;
-    /** Builds and returns a new User. */
-    private createNewUser();
-}
-/**
- * Data source to provide what data should be rendered in the table. Note that the data source
- * can retrieve its data in any way. In this case, the data source is provided a reference
- * to a common data base, ExampleDatabase. It is not the data source's responsibility to manage
- * the underlying data. Instead, it only needs to take the data and send the table exactly what
- * should be rendered.
- */
-export declare class ExampleDataSource extends DataSource<any> {
-    private _exampleDatabase;
-    private _paginator;
-    constructor(_exampleDatabase: ExampleDatabase, _paginator: MatPaginator);
-    /** Connect function called by the table to retrieve one stream containing the data to render. */
-    connect(): Observable<UserData[]>;
-    disconnect(): void;
+    position: number;
+    weight: number;
+    symbol: string;
 }
