fafe68643907726d3e5a0f707eebba6e0df31ce4
CDDelta <ngjunyen@gmail.com>
2018-01-25 18:50:33 +0000

    dbc34be docs(card): fix card overflowing on smaller screens (#9593)

--- examples-package/esm2015/material-examples.js
+++ examples-package/esm2015/material-examples.js
@@ -349,7 +349,7 @@ CardFancyExample.decorators = [
     { type: Component, args: [{
                 selector: 'card-fancy-example',
                 template: "<mat-card class=\"example-card\"><mat-card-header><div mat-card-avatar class=\"example-header-image\"></div><mat-card-title>Shiba Inu</mat-card-title><mat-card-subtitle>Dog Breed</mat-card-subtitle></mat-card-header><img mat-card-image src=\"https://material.angular.io/assets/img/examples/shiba2.jpg\" alt=\"Photo of a Shiba Inu\"><mat-card-content><p>The Shiba Inu is the smallest of the six original and distinct spitz breeds of dog from Japan. A small, agile dog that copes very well with mountainous terrain, the Shiba Inu was originally bred for hunting.</p></mat-card-content><mat-card-actions><button mat-button>LIKE</button> <button mat-button>SHARE</button></mat-card-actions></mat-card>",
-                styles: [".example-card { width: 400px; } .example-header-image { background-image: url('https://material.angular.io/assets/img/examples/shiba1.jpg'); background-size: cover; } "],
+                styles: [".example-card { max-width: 400px; } .example-header-image { background-image: url('https://material.angular.io/assets/img/examples/shiba1.jpg'); background-size: cover; } "],
             },] },
 ];
 /** @nocollapse */
