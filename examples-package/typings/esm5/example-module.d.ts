7d04906b50dbe4d6c7b15ce0ee1201c2daf6dd5c
Yuan Gao <tinagao@google.com>
2018-03-05 23:10:31 +0000

    5.2.x - c8ca770 fix(menu): Fix #10005 lint error

--- examples-package/typings/esm5/example-module.d.ts
+++ examples-package/typings/esm5/example-module.d.ts
@@ -1,17 +1,14 @@
-import { BottomSheetOverviewExampleSheet, BottomSheetOverviewExample } from './bottom-sheet-overview/bottom-sheet-overview-example';
 import { DatepickerLocaleExample } from './datepicker-locale/datepicker-locale-example';
 import { DialogContentExample } from './dialog-content/dialog-content-example';
 import { DialogOverviewExampleDialog } from './dialog-overview/dialog-overview-example';
 import { MyTelInput } from './form-field-custom-control/form-field-custom-control-example';
 import { FormFieldLabelExample } from './form-field-label/form-field-label-example';
-import { IconSvgExample } from './icon-svg/icon-svg-example';
+import { IconSvgExample } from './icon-svg-example/icon-svg-example';
 import { SidenavResponsiveExample } from './sidenav-responsive/sidenav-responsive-example';
 import { SlideToggleFormsExample } from './slide-toggle-forms/slide-toggle-forms-example';
 import { SnackBarOverviewExample } from './snack-bar-overview/snack-bar-overview-example';
 import { StepperOverviewExample } from './stepper-overview/stepper-overview-example';
 import { TableHttpExample } from './table-http/table-http-example';
-import { TreeFlatOverviewExample } from './tree-flat-overview/tree-flat-overview-example';
-import { TreeNestedOverviewExample } from './tree-nested-overview/tree-nested-overview-example';
 export interface LiveExample {
     title: string;
     component: any;
@@ -21,6 +18,6 @@ export interface LiveExample {
 export declare const EXAMPLE_COMPONENTS: {
     [key: string]: LiveExample;
 };
-export declare const EXAMPLE_LIST: (typeof BottomSheetOverviewExample | typeof BottomSheetOverviewExampleSheet | typeof DatepickerLocaleExample | typeof DialogContentExample | typeof DialogOverviewExampleDialog | typeof MyTelInput | typeof FormFieldLabelExample | typeof IconSvgExample | typeof SidenavResponsiveExample | typeof SlideToggleFormsExample | typeof SnackBarOverviewExample | typeof StepperOverviewExample | typeof TableHttpExample | typeof TreeFlatOverviewExample | typeof TreeNestedOverviewExample)[];
+export declare const EXAMPLE_LIST: (typeof DatepickerLocaleExample | typeof DialogContentExample | typeof DialogOverviewExampleDialog | typeof MyTelInput | typeof FormFieldLabelExample | typeof IconSvgExample | typeof SidenavResponsiveExample | typeof SlideToggleFormsExample | typeof SnackBarOverviewExample | typeof StepperOverviewExample | typeof TableHttpExample)[];
 export declare class ExampleModule {
 }
