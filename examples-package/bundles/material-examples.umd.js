64c515dceb500844797b692f258965d8ff3dfd20
Joey Perrott <josephperrott.github@gmail.com>
2018-03-29 15:31:45 +0000

    master - ddf22ed doc(button-toggle): add aria-label to button-toggle example (#10610)

--- examples-package/bundles/material-examples.umd.js
+++ examples-package/bundles/material-examples.umd.js
@@ -469,7 +469,7 @@ var ButtonToggleOverviewExample = /** @class */ (function () {
     ButtonToggleOverviewExample.decorators = [
         { type: core.Component, args: [{
                     selector: 'button-toggle-overview-example',
-                    template: "<mat-button-toggle-group name=\"fontStyle\"><mat-button-toggle value=\"bold\">Bold</mat-button-toggle><mat-button-toggle value=\"italic\">Italic</mat-button-toggle><mat-button-toggle value=\"underline\">Underline</mat-button-toggle></mat-button-toggle-group>",
+                    template: "<mat-button-toggle-group name=\"fontStyle\" aria-label=\"Font Style\"><mat-button-toggle value=\"bold\">Bold</mat-button-toggle><mat-button-toggle value=\"italic\">Italic</mat-button-toggle><mat-button-toggle value=\"underline\">Underline</mat-button-toggle></mat-button-toggle-group>",
                     styles: ["/** No CSS for this example */ "],
                 },] },
     ];
