83023978d26fe5cb99c5b405f0775bf662b22880
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2018-07-20 16:12:56 +0000

    master - f9d3757 chore(autocomplete): fix image spacing on overview example (#12286)

--- examples-package/bundles/material-examples.umd.js
+++ examples-package/bundles/material-examples.umd.js
@@ -460,7 +460,7 @@ var AutocompleteOverviewExample = /** @class */ (function () {
         { type: core.Component, args: [{
                     selector: 'autocomplete-overview-example',
                     template: "<form class=\"example-form\"><mat-form-field class=\"example-full-width\"><input matInput placeholder=\"State\" aria-label=\"State\" [matAutocomplete]=\"auto\" [formControl]=\"stateCtrl\"><mat-autocomplete #auto=\"matAutocomplete\"><mat-option *ngFor=\"let state of filteredStates | async\" [value]=\"state.name\"><img class=\"example-option-img\" aria-hidden [src]=\"state.flag\" height=\"25\"> <span>{{state.name}}</span> | <small>Population: {{state.population}}</small></mat-option></mat-autocomplete></mat-form-field><br><mat-slide-toggle [checked]=\"stateCtrl.disabled\" (change)=\"stateCtrl.disabled ? stateCtrl.enable() : stateCtrl.disable()\">Disable Input?</mat-slide-toggle></form>",
-                    styles: [".example-form { min-width: 150px; max-width: 500px; width: 100%; } .example-full-width { width: 100%; } .example-option-img { vertical-align: middle; } "],
+                    styles: [".example-form { min-width: 150px; max-width: 500px; width: 100%; } .example-full-width { width: 100%; } .example-option-img { vertical-align: middle; margin-right: 8px; } [dir='rtl'] .example-option-img { margin-right: 0; margin-left: 8px; } "],
                 },] },
     ];
     /** @nocollapse */
