94a89ee2a90e5225eaa752b0ab8158dd40e68803
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2018-08-21 19:01:10 +0000

    master - ecf8b0d fix(button-toggle): clickable area not stretching when custom width is set (#12642)

--- examples-package/esm5/material-examples.es5.js
+++ examples-package/esm5/material-examples.es5.js
@@ -3573,7 +3573,7 @@ var RippleOverviewExample = /** @class */ (function () {
         { type: Component, args: [{
                     selector: 'ripple-overview-example',
                     template: "<mat-checkbox [(ngModel)]=\"centered\" class=\"example-ripple-checkbox\">Centered</mat-checkbox><mat-checkbox [(ngModel)]=\"disabled\" class=\"example-ripple-checkbox\">Disabled</mat-checkbox><mat-checkbox [(ngModel)]=\"unbounded\" class=\"example-ripple-checkbox\">Unbounded</mat-checkbox><mat-form-field class=\"example-ripple-form-field\"><input matInput [(ngModel)]=\"radius\" type=\"number\" placeholder=\"Radius\"></mat-form-field><mat-form-field class=\"example-ripple-form-field\"><input matInput [(ngModel)]=\"color\" type=\"text\" placeholder=\"Color\"></mat-form-field><div class=\"example-ripple-container mat-elevation-z4\" matRipple [matRippleCentered]=\"centered\" [matRippleDisabled]=\"disabled\" [matRippleUnbounded]=\"unbounded\" [matRippleRadius]=\"radius\" [matRippleColor]=\"color\">Click me</div>",
-                    styles: [".example-ripple-container { cursor: pointer; text-align: center; width: 300px; height: 300px; line-height: 300px; } /** Styles to make the demo look better. */ .example-ripple-checkbox { margin: 6px 12px 6px 0; } .example-ripple-form-field { margin: 0 12px 0 0; } "],
+                    styles: [".example-ripple-container { cursor: pointer; text-align: center; width: 300px; height: 300px; line-height: 300px; -webkit-user-drag: none; -webkit-tap-highlight-color: transparent; } /** Styles to make the demo look better. */ .example-ripple-checkbox { margin: 6px 12px 6px 0; } .example-ripple-form-field { margin: 0 12px 0 0; } "],
                 },] },
     ];
     return RippleOverviewExample;
