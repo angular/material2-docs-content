84affa98cdba1760cece2f3a7d1b7426dd76cd21
Adrian PALUMBO <adrian.pal@me.com>
2018-10-04 19:04:48 +0000

    master - cdf8309 docs(form-field): invert "visibility" icon for password example (#13417)

--- examples-package/bundles/material-examples.umd.js
+++ examples-package/bundles/material-examples.umd.js
@@ -3494,7 +3494,7 @@ var FormFieldPrefixSuffixExample = /** @class */ (function () {
     FormFieldPrefixSuffixExample.decorators = [
         { type: core.Component, args: [{
                     selector: 'form-field-prefix-suffix-example',
-                    template: "<div class=\"example-container\"><mat-form-field><input matInput placeholder=\"Enter your password\" [type]=\"hide ? 'password' : 'text'\"><mat-icon matSuffix (click)=\"hide = !hide\">{{hide ? 'visibility' : 'visibility_off'}}</mat-icon></mat-form-field><mat-form-field><input matInput placeholder=\"Amount\" type=\"number\" class=\"example-right-align\"> <span matPrefix>$&nbsp;</span> <span matSuffix>.00</span></mat-form-field></div>",
+                    template: "<div class=\"example-container\"><mat-form-field><input matInput placeholder=\"Enter your password\" [type]=\"hide ? 'password' : 'text'\"><mat-icon matSuffix (click)=\"hide = !hide\">{{hide ? 'visibility_off' : 'visibility'}}</mat-icon></mat-form-field><mat-form-field><input matInput placeholder=\"Amount\" type=\"number\" class=\"example-right-align\"> <span matPrefix>$&nbsp;</span> <span matSuffix>.00</span></mat-form-field></div>",
                     styles: [".example-container { display: flex; flex-direction: column; } .example-container > * { width: 100%; } .example-right-align { text-align: right; } input.example-right-align::-webkit-outer-spin-button, input.example-right-align::-webkit-inner-spin-button { display: none; } input.example-right-align { -moz-appearance: textfield; } "],
                 },] },
     ];
