62bb68e79a133528bac2c25549d46214018f5da7
Andrew Seguin <andrewseguin@users.noreply.github.com>
2018-10-15 22:28:08 +0000

    master - 3f98306 chore: bump package.json to 7.0.0-rc.2 (#13623)

---
+++ examples-package/typings/esm5/cdk-virtual-scroll-custom-strategy/cdk-virtual-scroll-custom-strategy-example.d.ts
@@ -0,0 +1,8 @@
+import { FixedSizeVirtualScrollStrategy } from '@angular/cdk/scrolling';
+export declare class CustomVirtualScrollStrategy extends FixedSizeVirtualScrollStrategy {
+    constructor();
+}
+/** @title Virtual scroll with a custom strategy */
+export declare class CdkVirtualScrollCustomStrategyExample {
+    items: string[];
+}
