4f3b8bc246491e0461219eac7b13cdc1a1afac5f
Joey Perrott <josephperrott.github@gmail.com>
2018-10-19 20:53:17 +0000

    master - 59b47e1 doc(select): correct css styles in select panel custom styling example (#13691)

--- examples-package/esm5/material-examples.es5.js
+++ examples-package/esm5/material-examples.es5.js
@@ -4557,7 +4557,7 @@ var SelectPanelClassExample = /** @class */ (function () {
         { type: Component, args: [{
                     selector: 'select-panel-class-example',
                     template: "<mat-form-field><mat-select placeholder=\"Panel color\" [formControl]=\"panelColor\" panelClass=\"example-panel-{{panelColor.value}}\"><mat-option value=\"red\">Red</mat-option><mat-option value=\"green\">Green</mat-option><mat-option value=\"blue\">Blue</mat-option></mat-select></mat-form-field>",
-                    styles: [".example-panel-red .mat-select-panel { background: rgba(255, 0, 0, 0.5); } .example-panel-green .mat-select-panel { background: rgba(0, 255, 0, 0.5); } .example-panel-blue .mat-select-panel { background: rgba(0, 0, 255, 0.5); } "],
+                    styles: [".example-panel-red.mat-select-panel { background: rgba(255, 0, 0, 0.5); } .example-panel-green.mat-select-panel { background: rgba(0, 255, 0, 0.5); } .example-panel-blue.mat-select-panel { background: rgba(0, 0, 255, 0.5); } "],
                     // Encapsulation has to be disabled in order for the
                     // component style to apply to the select panel.
                     encapsulation: ViewEncapsulation.None,
