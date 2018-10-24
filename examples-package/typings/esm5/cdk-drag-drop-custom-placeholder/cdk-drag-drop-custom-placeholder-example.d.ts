db56f5249289342645e8a755c7efef2c1016c6fe
Paul Gschwendtner <paulgschwendtner@gmail.com>
2018-10-24 22:46:06 +0000

    master - e6a25f4 build: bazel and gulp tsconfigs need to be in sync (#13707)

--- examples-package/typings/esm5/cdk-drag-drop-custom-placeholder/cdk-drag-drop-custom-placeholder-example.d.ts
+++
@@ -1,8 +0,0 @@
-import { CdkDragDrop } from '@angular/cdk/drag-drop';
-/**
- * @title Drag&Drop custom placeholer
- */
-export declare class CdkDragDropCustomPlaceholderExample {
-    movies: string[];
-    drop(event: CdkDragDrop<string[]>): void;
-}
