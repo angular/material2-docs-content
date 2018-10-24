8f33346ced915e01a8f7898a17df29ddb8d03232
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2018-10-24 23:23:05 +0000

    master - bbeb5ef fix(stepper): incorrect border color in dark theme for header with label position (#13791)

---
+++ examples-package/typings/esm5/cdk-drag-drop-custom-placeholder/cdk-drag-drop-custom-placeholder-example.d.ts
@@ -0,0 +1,8 @@
+import { CdkDragDrop } from '@angular/cdk/drag-drop';
+/**
+ * @title Drag&Drop custom placeholer
+ */
+export declare class CdkDragDropCustomPlaceholderExample {
+    movies: string[];
+    drop(event: CdkDragDrop<string[]>): void;
+}
