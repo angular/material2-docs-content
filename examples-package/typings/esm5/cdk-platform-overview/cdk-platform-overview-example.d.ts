4ae55e16fb0df9829fdf2d5cbccc03f5d397cd67
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2018-10-29 15:22:53 +0000

    master - ab5127b docs: add example for cdk platform (#13821)

---
+++ examples-package/typings/esm5/cdk-platform-overview/cdk-platform-overview-example.d.ts
@@ -0,0 +1,11 @@
+import { Platform } from '@angular/cdk/platform';
+/**
+ * @title Platform overview
+ */
+export declare class CdkPlatformOverviewExample {
+    platform: Platform;
+    supportedInputTypes: string;
+    supportsPassiveEventListeners: boolean;
+    supportsScrollBehavior: boolean;
+    constructor(platform: Platform);
+}
