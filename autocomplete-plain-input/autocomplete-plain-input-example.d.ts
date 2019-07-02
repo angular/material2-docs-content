f744d4d69b6e1955074659dd5aa0ae6d8bfde551
Joey Perrott <josephperrott.github@gmail.com>
2019-07-02 20:50:08 +0000

    8.0.x - aa24d769c ci: update required statuses for PRs (#16394)

--- autocomplete-plain-input/autocomplete-plain-input-example.d.ts
+++ autocomplete-plain-input/autocomplete-plain-input-example.d.ts
@@ -4,7 +4,7 @@ import { Observable } from 'rxjs';
 /**
  * @title Plain input autocomplete
  */
-export declare class PlainInputAutocompleteExample implements OnInit {
+export declare class AutocompletePlainInputExample implements OnInit {
     control: FormControl;
     streets: string[];
     filteredStreets: Observable<string[]>;
