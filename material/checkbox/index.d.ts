8ca13bdc336ed84af4081acb96f5e5843b292700
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2020-04-17 16:33:16 +0000

    9.2.x - d564f6e09 fix(material-experimental/mdc-table): not supporting multiple themes (#18931)

--- material/checkbox/index.d.ts
+++ material/checkbox/index.d.ts
@@ -3,12 +3,13 @@ import { CheckboxOverviewExample } from './checkbox-overview/checkbox-overview-e
 import * as i0 from "@angular/core";
 import * as i1 from "./checkbox-configurable/checkbox-configurable-example";
 import * as i2 from "./checkbox-overview/checkbox-overview-example";
-import * as i3 from "@angular/material/card";
-import * as i4 from "@angular/material/checkbox";
-import * as i5 from "@angular/material/radio";
-import * as i6 from "@angular/forms";
+import * as i3 from "@angular/common";
+import * as i4 from "@angular/material/card";
+import * as i5 from "@angular/material/checkbox";
+import * as i6 from "@angular/material/radio";
+import * as i7 from "@angular/forms";
 export { CheckboxConfigurableExample, CheckboxOverviewExample, };
 export declare class CheckboxExamplesModule {
-    static ɵmod: i0.ɵɵNgModuleDefWithMeta<CheckboxExamplesModule, [typeof i1.CheckboxConfigurableExample, typeof i2.CheckboxOverviewExample], [typeof i3.MatCardModule, typeof i4.MatCheckboxModule, typeof i5.MatRadioModule, typeof i6.FormsModule], [typeof i1.CheckboxConfigurableExample, typeof i2.CheckboxOverviewExample]>;
+    static ɵmod: i0.ɵɵNgModuleDefWithMeta<CheckboxExamplesModule, [typeof i1.CheckboxConfigurableExample, typeof i2.CheckboxOverviewExample], [typeof i3.CommonModule, typeof i4.MatCardModule, typeof i5.MatCheckboxModule, typeof i6.MatRadioModule, typeof i7.FormsModule], [typeof i1.CheckboxConfigurableExample, typeof i2.CheckboxOverviewExample]>;
     static ɵinj: i0.ɵɵInjectorDef<CheckboxExamplesModule>;
 }
