72044c47dfa2b03f45d7fc2655db86caa3960c50
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2021-08-20 21:58:02 +0000

    12.2.x - 3bf50c892 docs(material/button-toggle): add selection mode example (#23415)

---
+++ material/button-toggle/button-toggle-mode/button-toggle-mode-example.d.ts
@@ -0,0 +1,8 @@
+import * as i0 from "@angular/core";
+/**
+ * @title Button toggle selection mode
+ */
+export declare class ButtonToggleModeExample {
+    static ɵfac: i0.ɵɵFactoryDeclaration<ButtonToggleModeExample, never>;
+    static ɵcmp: i0.ɵɵComponentDeclaration<ButtonToggleModeExample, "button-toggle-mode-example", never, {}, {}, never, never>;
+}
--- material/button-toggle/index.d.ts
+++ material/button-toggle/index.d.ts
@@ -3,18 +3,20 @@ import { ButtonToggleExclusiveExample } from './button-toggle-exclusive/button-t
 import { ButtonToggleOverviewExample } from './button-toggle-overview/button-toggle-overview-example';
 import { ButtonToggleHarnessExample } from './button-toggle-harness/button-toggle-harness-example';
 import { ButtonToggleFormsExample } from './button-toggle-forms/button-toggle-forms-example';
+import { ButtonToggleModeExample } from './button-toggle-mode/button-toggle-mode-example';
 import * as i0 from "@angular/core";
 import * as i1 from "./button-toggle-appearance/button-toggle-appearance-example";
 import * as i2 from "./button-toggle-exclusive/button-toggle-exclusive-example";
 import * as i3 from "./button-toggle-overview/button-toggle-overview-example";
 import * as i4 from "./button-toggle-harness/button-toggle-harness-example";
 import * as i5 from "./button-toggle-forms/button-toggle-forms-example";
-import * as i6 from "@angular/forms";
-import * as i7 from "@angular/material/button-toggle";
-import * as i8 from "@angular/material/icon";
-export { ButtonToggleAppearanceExample, ButtonToggleExclusiveExample, ButtonToggleOverviewExample, ButtonToggleHarnessExample, ButtonToggleFormsExample, };
+import * as i6 from "./button-toggle-mode/button-toggle-mode-example";
+import * as i7 from "@angular/forms";
+import * as i8 from "@angular/material/button-toggle";
+import * as i9 from "@angular/material/icon";
+export { ButtonToggleAppearanceExample, ButtonToggleExclusiveExample, ButtonToggleOverviewExample, ButtonToggleHarnessExample, ButtonToggleFormsExample, ButtonToggleModeExample, };
 export declare class ButtonToggleExamplesModule {
     static ɵfac: i0.ɵɵFactoryDeclaration<ButtonToggleExamplesModule, never>;
-    static ɵmod: i0.ɵɵNgModuleDeclaration<ButtonToggleExamplesModule, [typeof i1.ButtonToggleAppearanceExample, typeof i2.ButtonToggleExclusiveExample, typeof i3.ButtonToggleOverviewExample, typeof i4.ButtonToggleHarnessExample, typeof i5.ButtonToggleFormsExample], [typeof i6.FormsModule, typeof i7.MatButtonToggleModule, typeof i8.MatIconModule, typeof i6.ReactiveFormsModule], [typeof i1.ButtonToggleAppearanceExample, typeof i2.ButtonToggleExclusiveExample, typeof i3.ButtonToggleOverviewExample, typeof i4.ButtonToggleHarnessExample, typeof i5.ButtonToggleFormsExample]>;
+    static ɵmod: i0.ɵɵNgModuleDeclaration<ButtonToggleExamplesModule, [typeof i1.ButtonToggleAppearanceExample, typeof i2.ButtonToggleExclusiveExample, typeof i3.ButtonToggleOverviewExample, typeof i4.ButtonToggleHarnessExample, typeof i5.ButtonToggleFormsExample, typeof i6.ButtonToggleModeExample], [typeof i7.FormsModule, typeof i8.MatButtonToggleModule, typeof i9.MatIconModule, typeof i7.ReactiveFormsModule], [typeof i1.ButtonToggleAppearanceExample, typeof i2.ButtonToggleExclusiveExample, typeof i3.ButtonToggleOverviewExample, typeof i4.ButtonToggleHarnessExample, typeof i5.ButtonToggleFormsExample, typeof i6.ButtonToggleModeExample]>;
     static ɵinj: i0.ɵɵInjectorDeclaration<ButtonToggleExamplesModule>;
 }
