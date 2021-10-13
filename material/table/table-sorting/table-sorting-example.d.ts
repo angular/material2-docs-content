4e572b08ed0776e8d8eb6d81e291b7f0a9258bc9
Jeremy Elbourn <jelbourn@google.com>
2021-10-13 19:18:05 +0000

    master - ecd54f47d fix(material/sort): add description input for sort-header (#23633)

--- material/table/table-sorting/table-sorting-example.d.ts
+++ material/table/table-sorting/table-sorting-example.d.ts
@@ -1,5 +1,6 @@
+import { LiveAnnouncer } from '@angular/cdk/a11y';
 import { AfterViewInit } from '@angular/core';
-import { MatSort } from '@angular/material/sort';
+import { MatSort, Sort } from '@angular/material/sort';
 import { MatTableDataSource } from '@angular/material/table';
 import * as i0 from "@angular/core";
 export interface PeriodicElement {
@@ -12,10 +13,14 @@ export interface PeriodicElement {
  * @title Table with sorting
  */
 export declare class TableSortingExample implements AfterViewInit {
+    private _liveAnnouncer;
     displayedColumns: string[];
     dataSource: MatTableDataSource<PeriodicElement>;
+    constructor(_liveAnnouncer: LiveAnnouncer);
     sort: MatSort;
     ngAfterViewInit(): void;
+    /** Announce the change in sort state for assistive technology. */
+    announceSortChange(sortState: Sort): void;
     static ɵfac: i0.ɵɵFactoryDeclaration<TableSortingExample, never>;
     static ɵcmp: i0.ɵɵComponentDeclaration<TableSortingExample, "table-sorting-example", never, {}, {}, never, never>;
 }
