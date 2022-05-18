8afe917493c49a540da7fd0c5d6e3716cebb6b8b
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2022-05-18 11:57:27 +0000

    main - e5ae33712 refactor(material-experimental/mdc-slide-toggle): remove usage of MDC adapter (#24935)

--- esm2020/material/slide-toggle/slide-toggle-overview/slide-toggle-overview-example.mjs
+++ esm2020/material/slide-toggle/slide-toggle-overview/slide-toggle-overview-example.mjs
@@ -7,7 +7,7 @@ import * as i1 from "@angular/material/slide-toggle";
 export class SlideToggleOverviewExample {
 }
 SlideToggleOverviewExample.ɵfac = i0.ɵɵngDeclareFactory({ minVersion: "12.0.0", version: "14.0.0-rc.0", ngImport: i0, type: SlideToggleOverviewExample, deps: [], target: i0.ɵɵFactoryTarget.Component });
-SlideToggleOverviewExample.ɵcmp = i0.ɵɵngDeclareComponent({ minVersion: "14.0.0", version: "14.0.0-rc.0", type: SlideToggleOverviewExample, selector: "slide-toggle-overview-example", ngImport: i0, template: "<mat-slide-toggle>Slide me!</mat-slide-toggle>\n", dependencies: [{ kind: "component", type: i1.MatSlideToggle, selector: "mat-slide-toggle", inputs: ["disabled", "disableRipple", "color", "tabIndex", "name", "id", "labelPosition", "aria-label", "aria-labelledby", "aria-describedby", "required", "checked"], outputs: ["change", "toggleChange"], exportAs: ["matSlideToggle"] }] });
+SlideToggleOverviewExample.ɵcmp = i0.ɵɵngDeclareComponent({ minVersion: "14.0.0", version: "14.0.0-rc.0", type: SlideToggleOverviewExample, selector: "slide-toggle-overview-example", ngImport: i0, template: "<mat-slide-toggle>Slide me!</mat-slide-toggle>\n", dependencies: [{ kind: "component", type: i1.MatSlideToggle, selector: "mat-slide-toggle", inputs: ["disabled", "disableRipple", "color", "tabIndex"], exportAs: ["matSlideToggle"] }] });
 i0.ɵɵngDeclareClassMetadata({ minVersion: "12.0.0", version: "14.0.0-rc.0", ngImport: i0, type: SlideToggleOverviewExample, decorators: [{
             type: Component,
             args: [{ selector: 'slide-toggle-overview-example', template: "<mat-slide-toggle>Slide me!</mat-slide-toggle>\n" }]
