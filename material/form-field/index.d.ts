2bceb62cdc14a776f9d60110d4b25d187fc7c336
Miles Malerba <mmalerba@google.com>
2024-06-13 15:18:52 +0000

    main - 5ba7a0e docs(material/divider): Update divider examples (#29243)

--- material/form-field/index.d.ts
+++ material/form-field/index.d.ts
@@ -1,18 +1,18 @@
 import { AbstractControl } from '@angular/forms';
-import { BooleanInput } from '@angular/cdk/coercion';
 import { ControlValueAccessor } from '@angular/forms';
-import { ElementRef } from '@angular/core';
-import { FloatLabelType } from '@angular/material/form-field';
-import { FocusMonitor } from '@angular/cdk/a11y';
-import { FormBuilder } from '@angular/forms';
 import { FormControl } from '@angular/forms';
 import { FormGroup } from '@angular/forms';
 import * as i0 from '@angular/core';
+import { InputSignal } from '@angular/core';
+import { InputSignalWithTransform } from '@angular/core';
 import { MatFormField } from '@angular/material/form-field';
 import { MatFormFieldControl } from '@angular/material/form-field';
+import { ModelSignal } from '@angular/core';
 import { NgControl } from '@angular/forms';
 import { OnDestroy } from '@angular/core';
+import { Signal } from '@angular/core';
 import { Subject } from 'rxjs';
+import { WritableSignal } from '@angular/core';
 
 /** @title Form field appearance variants */
 export declare class FormFieldAppearanceExample {
@@ -22,15 +22,17 @@ export declare class FormFieldAppearanceExample {
 
 /** @title Form field with custom telephone number input control. */
 export declare class FormFieldCustomControlExample {
-    form: FormGroup;
+    readonly form: FormGroup<{
+        tel: FormControl<null>;
+    }>;
     static ɵfac: i0.ɵɵFactoryDeclaration<FormFieldCustomControlExample, never>;
     static ɵcmp: i0.ɵɵComponentDeclaration<FormFieldCustomControlExample, "form-field-custom-control-example", never, {}, {}, never, never, true, never>;
 }
 
 /** @title Form field with error messages */
 export declare class FormFieldErrorExample {
-    email: FormControl<string | null>;
-    errorMessage: string;
+    readonly email: FormControl<string | null>;
+    errorMessage: WritableSignal<string>;
     constructor();
     updateErrorMessage(): void;
     static ɵfac: i0.ɵɵFactoryDeclaration<FormFieldErrorExample, never>;
@@ -41,28 +43,29 @@ export declare class FormFieldErrorExample {
  * @title Testing with MatFormFieldHarness
  */
 export declare class FormFieldHarnessExample {
-    requiredControl: FormControl<string | null>;
+    readonly requiredControl: FormControl<string | null>;
     static ɵfac: i0.ɵɵFactoryDeclaration<FormFieldHarnessExample, never>;
     static ɵcmp: i0.ɵɵComponentDeclaration<FormFieldHarnessExample, "form-field-harness-example", never, {}, {}, never, never, true, never>;
 }
 
 /** @title Form field with hints */
 export declare class FormFieldHintExample {
+    protected readonly value: WritableSignal<string>;
+    protected onInput(event: Event): void;
     static ɵfac: i0.ɵɵFactoryDeclaration<FormFieldHintExample, never>;
     static ɵcmp: i0.ɵɵComponentDeclaration<FormFieldHintExample, "form-field-hint-example", never, {}, {}, never, never, true, never>;
 }
 
 /** @title Form field with label */
 export declare class FormFieldLabelExample {
-    private _formBuilder;
-    hideRequiredControl: FormControl<boolean | null>;
-    floatLabelControl: FormControl<"always" | "auto" | null>;
-    options: FormGroup<    {
+    readonly hideRequiredControl: FormControl<boolean | null>;
+    readonly floatLabelControl: FormControl<"always" | "auto" | null>;
+    readonly options: FormGroup<    {
     hideRequired: FormControl<boolean | null>;
     floatLabel: FormControl<"always" | "auto" | null>;
     }>;
-    constructor(_formBuilder: FormBuilder);
-    getFloatLabelValue(): FloatLabelType;
+    protected readonly hideRequired: Signal<boolean | null | undefined>;
+    protected readonly floatLabel: Signal<"always" | "auto">;
     static ɵfac: i0.ɵɵFactoryDeclaration<FormFieldLabelExample, never>;
     static ɵcmp: i0.ɵɵComponentDeclaration<FormFieldLabelExample, "form-field-label-example", never, {}, {}, never, never, true, never>;
 }
@@ -75,17 +78,10 @@ export declare class FormFieldOverviewExample {
 
 /** @title Form field with prefix & suffix */
 export declare class FormFieldPrefixSuffixExample {
-    hide: boolean;
+    hide: WritableSignal<boolean>;
     clickEvent(event: MouseEvent): void;
     static ɵfac: i0.ɵɵFactoryDeclaration<FormFieldPrefixSuffixExample, never>;
     static ɵcmp: i0.ɵɵComponentDeclaration<FormFieldPrefixSuffixExample, "form-field-prefix-suffix-example", never, {}, {}, never, never, true, never>;
-}
-
-/** @title Form field theming */
-export declare class FormFieldThemingExample {
-    colorControl: FormControl<"primary" | "accent" | "warn" | null | undefined>;
-    static ɵfac: i0.ɵɵFactoryDeclaration<FormFieldThemingExample, never>;
-    static ɵcmp: i0.ɵɵComponentDeclaration<FormFieldThemingExample, "form-field-theming-example", never, {}, {}, never, never, true, never>;
 }
 
 /** Data structure for holding telephone number. */
@@ -98,44 +94,45 @@ declare class MyTel {
 
 /** Custom `MatFormFieldControl` for telephone number input. */
 export declare class MyTelInput implements ControlValueAccessor, MatFormFieldControl<MyTel>, OnDestroy {
-    private _focusMonitor;
-    private _elementRef;
-    _formField: MatFormField;
-    ngControl: NgControl;
     static nextId: number;
-    areaInput: HTMLInputElement;
-    exchangeInput: HTMLInputElement;
-    subscriberInput: HTMLInputElement;
-    parts: FormGroup<{
+    readonly areaInput: Signal<HTMLInputElement>;
+    readonly exchangeInput: Signal<HTMLInputElement>;
+    readonly subscriberInput: Signal<HTMLInputElement>;
+    ngControl: NgControl | null;
+    readonly parts: FormGroup<{
         area: FormControl<string | null>;
         exchange: FormControl<string | null>;
         subscriber: FormControl<string | null>;
     }>;
-    stateChanges: Subject<void>;
-    focused: boolean;
-    touched: boolean;
-    controlType: string;
-    id: string;
+    readonly stateChanges: Subject<void>;
+    readonly touched: WritableSignal<boolean>;
+    readonly controlType = "example-tel-input";
+    readonly id: string;
+    readonly _userAriaDescribedBy: InputSignal<string>;
+    readonly _placeholder: InputSignal<string>;
+    readonly _required: InputSignalWithTransform<boolean, unknown>;
+    readonly _disabledByInput: InputSignalWithTransform<boolean, unknown>;
+    readonly _value: ModelSignal<MyTel | null>;
     onChange: (_: any) => void;
     onTouched: () => void;
+    protected readonly _formField: MatFormField | null;
+    private readonly _focused;
+    private readonly _disabledByCva;
+    private readonly _disabled;
+    private readonly _focusMonitor;
+    private readonly _elementRef;
+    get focused(): boolean;
     get empty(): boolean;
     get shouldLabelFloat(): boolean;
-    userAriaDescribedBy: string;
+    get userAriaDescribedBy(): string;
     get placeholder(): string;
-    set placeholder(value: string);
-    private _placeholder;
     get required(): boolean;
-    set required(value: BooleanInput);
-    private _required;
     get disabled(): boolean;
-    set disabled(value: BooleanInput);
-    private _disabled;
     get value(): MyTel | null;
-    set value(tel: MyTel | null);
     get errorState(): boolean;
-    constructor(formBuilder: FormBuilder, _focusMonitor: FocusMonitor, _elementRef: ElementRef<HTMLElement>, _formField: MatFormField, ngControl: NgControl);
+    constructor();
     ngOnDestroy(): void;
-    onFocusIn(event: FocusEvent): void;
+    onFocusIn(): void;
     onFocusOut(event: FocusEvent): void;
     autoFocusNext(control: AbstractControl, nextElement?: HTMLInputElement): void;
     autoFocusPrev(control: AbstractControl, prevElement: HTMLInputElement): void;
@@ -146,8 +143,9 @@ export declare class MyTelInput implements ControlValueAccessor, MatFormFieldCon
     registerOnTouched(fn: any): void;
     setDisabledState(isDisabled: boolean): void;
     _handleInput(control: AbstractControl, nextElement?: HTMLInputElement): void;
-    static ɵfac: i0.ɵɵFactoryDeclaration<MyTelInput, [null, null, null, { optional: true; }, { optional: true; self: true; }]>;
-    static ɵcmp: i0.ɵɵComponentDeclaration<MyTelInput, "example-tel-input", never, { "userAriaDescribedBy": { "alias": "aria-describedby"; "required": false; }; "placeholder": { "alias": "placeholder"; "required": false; }; "required": { "alias": "required"; "required": false; }; "disabled": { "alias": "disabled"; "required": false; }; "value": { "alias": "value"; "required": false; }; }, {}, never, never, true, never>;
+    private _updateValue;
+    static ɵfac: i0.ɵɵFactoryDeclaration<MyTelInput, never>;
+    static ɵcmp: i0.ɵɵComponentDeclaration<MyTelInput, "example-tel-input", never, { "_userAriaDescribedBy": { "alias": "aria-describedby"; "required": false; "isSignal": true; }; "_placeholder": { "alias": "placeholder"; "required": false; "isSignal": true; }; "_required": { "alias": "required"; "required": false; "isSignal": true; }; "_disabledByInput": { "alias": "disabled"; "required": false; "isSignal": true; }; "_value": { "alias": "value"; "required": false; "isSignal": true; }; }, { "_value": "valueChange"; }, never, never, true, never>;
 }
 
 export { }
