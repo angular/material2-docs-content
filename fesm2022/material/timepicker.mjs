2430f72ef50ef9bc458845a15847d37d1ef6d860
Joey Perrott <josephperrott@gmail.com>
2025-04-22 18:18:35 +0000

    19.2.x - 89db9f7 [PATCH] Migrate to use rules_js based ng_package (#30917)

--- fesm2022/material/timepicker.mjs
+++ fesm2022/material/timepicker.mjs
@@ -1,5 +1,5 @@
 import * as i0 from '@angular/core';
-import { ChangeDetectionStrategy, Component, inject, signal } from '@angular/core';
+import { Component, ChangeDetectionStrategy, inject, signal } from '@angular/core';
 import * as i3 from '@angular/material/timepicker';
 import { MatTimepickerModule } from '@angular/material/timepicker';
 import * as i2 from '@angular/material/input';
