c063be00f7af0c3b5b2a56d29dd50fb09b855199
Wagner Maciel <wagnermaciel@google.com>
2025-11-04 21:52:48 +0000

    main - ccd0e07 refactor(aria/toolbar): large rework of toolbar internals (#32236)

--- types/aria-toolbar.d.ts
+++ types/aria-toolbar.d.ts
@@ -1,63 +1,42 @@
-import { LiveAnnouncer } from '@angular/cdk/a11y';
 import * as i0 from '@angular/core';
-import { FormControl } from '@angular/forms';
 
 /** @title Basic Horizontal Toolbar Example */
 declare class ToolbarBasicHorizontalExample {
-    private _liveAnnouncer;
-    constructor(_liveAnnouncer: LiveAnnouncer);
-    format(tool: string): void;
-    test(action: string): void;
     static ɵfac: i0.ɵɵFactoryDeclaration<ToolbarBasicHorizontalExample, never>;
     static ɵcmp: i0.ɵɵComponentDeclaration<ToolbarBasicHorizontalExample, "toolbar-basic-horizontal-example", never, {}, {}, never, never, true, never>;
 }
 
 /** @title Basic Vertical Toolbar Example */
 declare class ToolbarBasicVerticalExample {
-    private _liveAnnouncer;
-    constructor(_liveAnnouncer: LiveAnnouncer);
-    format(tool: string): void;
-    test(action: string): void;
     static ɵfac: i0.ɵɵFactoryDeclaration<ToolbarBasicVerticalExample, never>;
     static ɵcmp: i0.ɵɵComponentDeclaration<ToolbarBasicVerticalExample, "toolbar-basic-vertical-example", never, {}, {}, never, never, true, never>;
 }
 
-/** @title Configurable CDK Radio Group */
+/** @title Configurable Aria Toolbar Example */
 declare class ToolbarConfigurableExample {
-    softDisabled: FormControl<boolean>;
-    wrap: FormControl<boolean>;
-    toolbarDisabled: FormControl<boolean>;
+    wrap: boolean;
+    softDisabled: boolean;
+    toolbarDisabled: boolean;
     orientation: 'vertical' | 'horizontal';
-    fruits: string[];
-    buttonFruits: string[];
-    disabled: FormControl<boolean>;
-    readonly: FormControl<boolean>;
-    disabledOptions: string[];
-    disabledButtonOptions: string[];
-    test(x: String): void;
+    widgets: string[];
+    groups: string[];
+    disabledGroups: string[];
+    disabledWidgets: string[];
+    isDisabled(value: string): boolean;
     static ɵfac: i0.ɵɵFactoryDeclaration<ToolbarConfigurableExample, never>;
     static ɵcmp: i0.ɵɵComponentDeclaration<ToolbarConfigurableExample, "toolbar-configurable-example", never, {}, {}, never, never, true, never>;
 }
 
 /** @title Basic RTL Toolbar Example */
 declare class ToolbarRtlExample {
-    private _liveAnnouncer;
-    constructor(_liveAnnouncer: LiveAnnouncer);
-    format(tool: string): void;
-    test(action: string): void;
     static ɵfac: i0.ɵɵFactoryDeclaration<ToolbarRtlExample, never>;
     static ɵcmp: i0.ɵɵComponentDeclaration<ToolbarRtlExample, "toolbar-rtl-example", never, {}, {}, never, never, true, never>;
 }
 
-/** @title Soft Disabled Toolbar Example */
-declare class ToolbarSoftDisabledExample {
-    private _liveAnnouncer;
-    constructor(_liveAnnouncer: LiveAnnouncer);
-    disabledOptions: string[];
-    format(tool: string): void;
-    test(action: string): void;
-    static ɵfac: i0.ɵɵFactoryDeclaration<ToolbarSoftDisabledExample, never>;
-    static ɵcmp: i0.ɵɵComponentDeclaration<ToolbarSoftDisabledExample, "toolbar-soft-disabled-example", never, {}, {}, never, never, true, never>;
+/** @title Hard Disabled Toolbar Example */
+declare class ToolbarHardDisabledExample {
+    static ɵfac: i0.ɵɵFactoryDeclaration<ToolbarHardDisabledExample, never>;
+    static ɵcmp: i0.ɵɵComponentDeclaration<ToolbarHardDisabledExample, "toolbar-hard-disabled-example", never, {}, {}, never, never, true, never>;
 }
 
-export { ToolbarBasicHorizontalExample, ToolbarBasicVerticalExample, ToolbarConfigurableExample, ToolbarRtlExample, ToolbarSoftDisabledExample };
+export { ToolbarBasicHorizontalExample, ToolbarBasicVerticalExample, ToolbarConfigurableExample, ToolbarHardDisabledExample, ToolbarRtlExample };
