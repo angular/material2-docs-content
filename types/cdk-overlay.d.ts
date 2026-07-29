af5114497e03171fd37c9c7268adf685120d4bdf
Michael Small <85510853+msmallest@users.noreply.github.com>
2026-07-29 09:48:38 +0000

    22.0.x - fedb011 docs: convert CDK overlay to signals (#33591)

--- types/cdk-overlay.d.ts
+++ types/cdk-overlay.d.ts
@@ -4,7 +4,7 @@ import * as i0 from '@angular/core';
  * @title Overlay basic example
  */
 declare class CdkOverlayBasicExample {
-    isOpen: boolean;
+    isOpen: i0.WritableSignal<boolean>;
     static ɵfac: i0.ɵɵFactoryDeclaration<CdkOverlayBasicExample, never>;
     static ɵcmp: i0.ɵɵComponentDeclaration<CdkOverlayBasicExample, "cdk-overlay-basic-example", never, {}, {}, never, never, true, never>;
 }
